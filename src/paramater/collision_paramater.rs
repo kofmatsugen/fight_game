3e4069d2fe3247eea685aad186ca9eaf844d0828
Kaneyuki Hiromasa <matsugen2511@gmail.com>
2020-03-29 22:10:38 +0900

    判定表示のデバッグの色を指定

--- src/paramater/collision_paramater.rs
+++ src/paramater/collision_paramater.rs
@@ -2,16 +2,30 @@ use crate::{
     paramater::{AnimationParam, CollisionType},
     traits::ParamaterFromData,
 };
+#[cfg(feature = "debug")]
+use amethyst_collision::traits::debug::collision_color::CollisionColor;
 
 #[derive(Clone, Copy, Debug)]
 pub struct CollisionParamater {
-    _collision_type: CollisionType,
+    collision_type: CollisionType,
 }
 
 impl ParamaterFromData<AnimationParam> for CollisionParamater {
     fn make_collision_data(param: Option<&AnimationParam>) -> Option<Self> {
-        let _collision_type = param?.collision_type?;
+        let collision_type = param?.collision_type?;
 
-        Some(CollisionParamater { _collision_type })
+        Some(CollisionParamater { collision_type })
+    }
+}
+
+#[cfg(feature = "debug")]
+impl CollisionColor for CollisionParamater {
+    fn collision_color(&self) -> (f32, f32, f32, f32) {
+        match self.collision_type {
+            CollisionType::Extrusion => (1., 0., 1., 1.),
+            CollisionType::Blow => (1., 0., 0., 1.),
+            CollisionType::Projectile => (0., 1., 0., 1.),
+            CollisionType::Throw => (0., 0., 1., 1.),
+        }
     }
 }
